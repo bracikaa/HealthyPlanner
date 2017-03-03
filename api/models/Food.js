a329cfe584dee58a34121082c4f95b4ba8883cb6
bracikaa <mehmed_duhovic@hotmail.com>
2017-03-03 22:42:45 +0100

    updated user

--- api/models/Food.js
+++ api/models/Food.js
@@ -8,6 +8,52 @@
 module.exports = {
 
   attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+
+    calories: {
+      type: 'int',
+      required: true
+    },
+
+    fat:{
+      type: 'double',
+      required: true
+    },
+
+    protein:{
+      type: 'double',
+      required: true
+    },
+
+    fiber: {
+      type: 'double',
+      required: true
+    },
+
+    carbohydrates: {
+      type: 'double',
+      required: true
+    },
+
+    sugars: {
+      type: 'double',
+      required: true
+    },
+
+    vitamins: {
+      type: 'json',
+      required: true
+    },
+
+    mineral: {
+      type: 'json',
+      required: true
+    }
+
+
 
   }
 };
