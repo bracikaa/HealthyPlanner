60936d78f85fab3ab13f1d56e1b85bd3aa2845b5
bracikaa <mehmed_duhovic@hotmail.com>
2017-03-03 23:23:14 +0100

    nešto novo

---
+++ api/controllers/MineralsController.js
@@ -0,0 +1,11 @@
+/**
+ * MineralsController
+ *
+ * @description :: Server-side logic for managing Minerals
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/VitaminsController.js
@@ -0,0 +1,11 @@
+/**
+ * VitaminsController
+ *
+ * @description :: Server-side logic for managing Vitamins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
