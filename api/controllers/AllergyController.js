1b92d14a98c29caa3eaef3951be706c9f59412e7
bracikaa <mehmed_duhovic@hotmail.com>
2017-03-04 02:40:50 +0100

    nešto novo

--- api/controllers/AllergyController.js
+++ api/controllers/AllergyController.js
@@ -6,6 +6,48 @@
  */
 
 module.exports = {
-	
+  index: function (req, res) {
+    Allergy.find().exec(function (err, data) {
+      return res.json(data);
+    });
+  },
+
+  create: function (req, res) {
+
+    Allergy.create(req.body).exec(function allergyCreated(err, allergy) {
+      if (err) {
+        console.log(err);
+        return res.send(err, 500);
+      }
+
+      return res.json(allergy);
+    });
+  },
+
+  destroy: function(req, res){
+    Allergy.destroy(req.query).exec(function (err){
+      if(err){
+        console.log(err);
+      }
+      else
+      {
+        console.log("Allergy Destroyed");
+      }
+    });
+  },
+
+  show: function(req, res){
+    Allergy.find(req.query).exec(function (err, allergy) {
+      if(err) return res.send(err, 500);
+      return res.json(allergy);
+    });
+  },
+
+  update: function(req, res){
+    Allergy.update(req.query, req.body).exec(function (err, allergy) {
+      if(err) return res.send(err, 500);
+      return res.json(allergy);
+    });
+  }
 };
 
--- api/controllers/DiagnoseController.js
+++ api/controllers/DiagnoseController.js
@@ -6,6 +6,49 @@
  */
 
 module.exports = {
-	
+  index: function (req, res) {
+    Diagnose.find().exec(function (err, data) {
+      return res.json(data);
+    });
+  },
+
+  create: function (req, res) {
+
+    Diagnose.create(req.body).exec(function diagnose(err, diagnose) {
+      if (err) {
+        console.log(err);
+        return res.send(err, 500);
+      }
+
+      return res.json(diagnose);
+    });
+  },
+
+  destroy: function(req, res){
+    Diagnose.destroy(req.query).exec(function (err){
+      if(err){
+        console.log(err);
+      }
+      else
+      {
+        console.log("Diagnose Destroyed");
+      }
+    })
+  },
+
+  show: function(req, res){
+    Diagnose.find(req.query).exec(function (err, diagnose) {
+      if(err) return res.send(err, 500);
+      return res.json(diagnose);
+    });
+  },
+
+  update: function(req, res){
+    Diagnose.update(req.query, req.body).exec(function (err, diagnose) {
+      if(err) return res.send(err, 500);
+      return res.json(diagnose);
+    });
+  }
+
 };
 
