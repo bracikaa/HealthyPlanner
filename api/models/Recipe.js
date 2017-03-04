15244eec5f45dca62d40465c87f006486e373563
Kemoke <mail@kemoke.net>
2017-03-04 08:05:39 +0100

    fixes

--- api/models/Recipe.js
+++ api/models/Recipe.js
@@ -10,7 +10,7 @@ module.exports = {
     attributes: {
         ingredients: {
             collection: 'recipeFood',
-            embed: true
+            via: 'recipe'
         },
 
         name: {
